fcee0e9c1cd3ad6fd5c950aa1a6280029681ce65
pzaldivar94@outlook.com <pzaldivar94@outlook.com>
2023-09-30 14:30:10 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Tabbed Menu
+function openMenu(event, menuName) {
+    let menuArray = document.getElementsByClassName("menu")
+    for (i = 0; i < menuArray.length; i++) {
+        menuArray[i].style.display = 'none'
+    }
+    let tablinks = document.getElementsByClassName("tablink")
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove('active-tab')
+    }
+
+    document.getElementById(menuName).style.display = 'block'
+    event.currentTarget.classList.add('active-tab')
+}
+
+document.getElementById("mainlink").click()
